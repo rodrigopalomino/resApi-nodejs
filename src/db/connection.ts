87c7cecc3806a7ae8b0230af44cc23961dfbf86f
rodrigopalomino <117995998+rodrigopalomino@users.noreply.github.com>
2023-08-06 13:21:02 -0500

    3

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || "",
   {
     host: process.env.DB_HOST || "localhost",
+    port: Number(process.env.DB_PORT),
     dialect: "mysql",
   }
 );
